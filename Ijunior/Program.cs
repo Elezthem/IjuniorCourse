b8be967d6c31d31c7cc8a0a8c6fcfbeea42b2ee4
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-22 13:02:22 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,19 @@
+﻿string name;
+string age;
+string programCount;
+string money;
+string time;
+string truth;
+
+name = "Sergey";
+age = "16";
+programCount = "CubeTower";
+money = "4000";
+time = "300";
+truth = "YES!";
+
+Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+Console.WriteLine("Мне " + age + " лет.");
+Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
+Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунд.");
+Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
